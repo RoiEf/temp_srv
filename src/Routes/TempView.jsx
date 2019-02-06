500f2841d4d435c8cfb3475051dc83a094a397bb
RoiEf <roi.efrati@gmail.com>
2019-02-06 11:57:40 +0200

    Save on exit

--- src/Routes/TempView.jsx
+++ src/Routes/TempView.jsx
@@ -1,10 +1,43 @@
-import React from "react";
-import { Alert } from "react-bootstrap";
+import React, { Component } from "react";
+import { Jumbotron } from "react-bootstrap";
+import {Line} from 'react-chartjs-2';
 
-const TempView = () => {
+class TempView extends Component {
+
+  state = {
+    chartData: {
+      labels: [0,1,2,3,4,5,6,7,8,9],
+      datasets: [
+        {
+          label: 'Temprature',
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          data: [10,20,30,40,30,50,60,70,50,80]
+        },
+        {
+          label: 'Humidity',
+          backgroundColor: 'pink',
+          data: [30,40,40,50,50,60,70,80,70,85]
+        },
+      ]
+    }
+  };
+
+  render () {
   return (
-      <Alert variant={"primary"}>Temp & Humidity</Alert>
+    <React.Fragment>
+      <Jumbotron>
+        <h1>Temp & Humidity Dashboard</h1>
+      </Jumbotron>
+      <Jumbotron>
+        <Line 
+          data={this.state.chartData}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </Jumbotron>
+    </React.Fragment>
   );
-};
+}}
 
 export default TempView;
